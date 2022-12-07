189613132d81642691cc6f395cc04a3657caa2aa
digitalrenew <112146234+digitalrenew@users.noreply.github.com>
2022-12-07 10:13:06 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+    <div>
+        <h1>Welcome to the website</h1>
+        <ol>
+            <li>lol</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(page,document.getElementById("root"))
